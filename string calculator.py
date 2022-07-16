b71cab64bde18f1d575d7fab5cfda73e762a4330
amanramoliya <70715941+amanramoliya@users.noreply.github.com>
2022-07-16 15:48:31 +0530

    Update string calculator.py

--- string calculator.py
+++ string calculator.py
@@ -4,7 +4,12 @@ def string_Calculator(string_numbers):
         return 0
     else:
         string_numbers = map(int, re.findall(r"-?\d+", string_numbers))
-        string_numbers = list(filter(lambda x: x < 1000, string_numbers))
+        string_numbers = list(string_numbers)
+        print(string_numbers)
         negative_numbers = list(filter(lambda x: x < 0, string_numbers))
         if negative_numbers: raise Exception('negatives not allowed ' + str(negative_numbers))
-        return sum(string_numbers)
+        ans=0            
+        for i, val in enumerate(string_numbers):
+            if i%2==0 and val<1000:
+                ans=ans+val
+        return ans
