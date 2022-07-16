1344a01ef847c4475d9a57d7531e8eb7a14d94fe
amanramoliya <70715941+amanramoliya@users.noreply.github.com>
2022-07-16 12:14:53 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,39 +2,39 @@ import string_calculator as calc
 import pytest
 
 def test_empty_string():
-    assert calc.add('') == 0
+    assert calc.string_Calculator('') == 0
 
 def test_single_number_string():
-    assert calc.add('6') == 6
-    assert calc.add('0') == 0
+    assert calc.string_Calculator('6') == 6
+    assert calc.string_Calculator('0') == 0
           
 def test_two_numbers_string():
-    assert calc.add('1,4') == 5
-    assert calc.add('21,21') == 42
+    assert calc.string_Calculator('1,4') == 5
+    assert calc.string_Calculator('21,21') == 42
 
 def test_multiple_numbers_string():
-    assert calc.add('1,2,3') == 6
-    assert calc.add('100,200,300,50,10,40') == 700
+    assert calc.string_Calculator('1,2,3') == 6
+    assert calc.string_Calculator('100,200,300,50,10,40') == 700
 
 def test_new_line_between_numbers_string():
-    assert calc.add('1\n2,3') == 6
-    assert calc.add('10\n20\n20\n10,20\n20') == 100
+    assert calc.string_Calculator('1\n2,3') == 6
+    assert calc.string_Calculator('10\n20\n20\n10,20\n20') == 100
 
 def test_different_delimiter():
-    assert calc.add('//;\n1;2') == 3
-    assert calc.add('//;\n1;2;7\n10') == 20
+    assert calc.string_Calculator('//;\n1;2') == 3
+    assert calc.string_Calculator('//;\n1;2;7\n10') == 20
 
 def test_ignore_larger_than_1000():
-    assert calc.add('1000,100') == 100
-    assert calc.add('1\n2,3000') == 3
-    assert calc.add('//+\n1+2+7\n980') == 990
+    assert calc.string_Calculator('1000,100') == 100
+    assert calc.string_Calculator('1\n2,3000') == 3
+    assert calc.string_Calculator('//+\n1+2+7\n980') == 990
 
 def test_any_length_delimiter():
-    assert calc.add('//***\n10***20***70\n1000') == 100
-    assert calc.add('//++\n1++2++7\n10') == 20
+    assert calc.string_Calculator('//***\n10***20***70\n1000') == 100
+    assert calc.string_Calculator('//++\n1++2++7\n10') == 20
 
 def test_multiple_delimiters():
-    assert calc.add('//+*\n1+2*7') == 10
+    assert calc.string_Calculator('//+*\n1+2*7') == 10
     assert calc.add('//+%\n1+2%7\n10') == 20
 
 def test_handle_negative_numbers():
